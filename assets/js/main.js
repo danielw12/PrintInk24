268c4139a4373177cbddb3269148a7b011deee14
danielw12 <33023142+danielw12@users.noreply.github.com>
2020-03-09 18:23:05 +0100

    fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -50,68 +50,68 @@ $(document).scroll(function() {
   }
 });
 
-$("#offerElement").click(function(){
-  $("#offerElement").toggleClass("invert");
+$("#offerElement1").click(function(){
+  $("#bg1").toggleClass("invert");
     $("#offerElement>.row>.info>p").toggleClass("weight");
 });
 
 $("#offerElement2").click(function(){
-  $("#offerElement2").toggleClass("invert");
+  $("#bg2").toggleClass("invert");
     $("#offerElement2>.row>.info>p").toggleClass("weight");
 });
 
 $("#offerElement3").click(function(){
-  $("#offerElement3").toggleClass("invert");
+  $("#bg3").toggleClass("invert");
     $("#offerElement3>.row>.info>p").toggleClass("weight");
 });
 
 
 $("#offerElement4").click(function(){
-  $("#offerElement4").toggleClass("invert");
+  $("#bg4").toggleClass("invert");
     $("#offerElement4>.row>.info>p").toggleClass("weight");
 });
 
 
 $("#offerElement5").click(function(){
-  $("#offerElement5").toggleClass("invert");
+  $("#bg5").toggleClass("invert");
     $("#offerElement5>.row>.info>p").toggleClass("weight");
 });
 
 $("#offerElement6").click(function(){
-  $("#offerElement6").toggleClass("invert");
+  $("#bg6").toggleClass("invert");
     $("#offerElement6>.row>.info>p").toggleClass("weight");
 });
 
 $("#offerElement7").click(function(){
-  $("#offerElement7").toggleClass("invert");
+  $("#bg7").toggleClass("invert");
     $("#offerElement7>.row>.info>p").toggleClass("weight");
 });
 
 $("#offerElement8").click(function(){
-  $("#offerElement8").toggleClass("invert");
+  $("#bg8").toggleClass("invert");
     $("#offerElement8>.row>.info>p").toggleClass("weight");
 });
 
 
 $("#offerElement9").click(function(){
-  $("#offerElement9").toggleClass("invert");
+  $("#bg9").toggleClass("invert");
     $("#offerElement9>.row>.info>p").toggleClass("weight");
 });
 
 
 $("#offerElement10").click(function(){
-  $("#offerElement10").toggleClass("invert");
+  $("#bg10").toggleClass("invert");
     $("#offerElement10>.row>.info>p").toggleClass("weight");
 });
 
 
 $("#offerElement11").click(function(){
-  $("#offerElement11").toggleClass("invert");
+  $("#bg11").toggleClass("invert");
     $("#offerElement11>.row>.info>p").toggleClass("weight");
 });
 
 $("#offerElement12").click(function(){
-  $("#offerElement12").toggleClass("invert");
+  $("#bg12").toggleClass("invert");
     $("#offerElement12>.row>.info>p").toggleClass("weight");
 });
 
