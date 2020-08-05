3e46b314cecb2ca0be31802ebcea84062d5f6a6a
danielw12 <33023142+danielw12@users.noreply.github.com>
2020-08-05 11:36:54 +0200

    reactivate slick

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,14 @@
 
-//$(document).ready(function(){
-//  $('.responsive').slick({
-//   infinite: true,
-//  slidesToShow: 1,
-//  slidesToScroll: 1,
-//      autoplay: true,
-//      autoplaySpeed: 10000,
-//      arrows: false
-//});
-//    });
+$(document).ready(function(){
+  $('.responsive').slick({
+   infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 10000,
+      arrows: false
+});
+    });
 
 
 $(document).ready(function(){
